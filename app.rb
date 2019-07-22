cdd653956d0b5bd1215e4bfbfd11452c40e6791a
aparkening <aaron.parkening@gmail.com>
2019-07-22 15:13:41 -0400

    Done. Insert form and test with Capybara.

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 class Application < Sinatra::Base
-  # Write your code here!
+
+  # Index page; show form
+  get '/' do
+    erb :index
+  end
+
+  # Respond to the form submission
+  post '/greet' do
+    erb :greet
+  end
 
 end
